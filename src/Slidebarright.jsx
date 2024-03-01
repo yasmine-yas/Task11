8130a207845234bc61387c14fcc931a645364d98
yasmine muhamed <74177304+yasmine-yas@users.noreply.github.com>
2024-03-01 03:39:03 +0200

    y

--- src/Slidebarright.jsx
+++ src/Slidebarright.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 function Slidebarright() {
   return (
-    <div className='main-title'>
-      <uul>
-        <li>Filters</li>
-      </uul>
+    <div className='sidebar-right'>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+        <p>Filters</p>
+      </div>
+      <div style={{ marginBottom: '20px' }}></div>
+      <ul>
+        <li className="pair-item">Health Care</li>
+        <li className="pair-item">IT</li>
+        <li className="pair-item">Materials</li>
+        <li className="pair-item">Communication</li>
+        <li className="pair-item">Energy</li>
+        <li className="pair-item">Industrials</li>
+        <li className="pair-item">Consumer Discretionary</li>
+        <li className="pair-item">Utilities</li>
+        <li className="pair-item">Consumer Staples</li>
+        <li className="pair-item">Financials</li>
+        <li className="pair-item">Real Estate</li>
+      </ul>
+      <div style={{ marginBottom: '20px' }}></div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+        <button className="blue-button">Apply</button>
+      </div>
+      <div style={{ marginBottom: '20px' }}></div>
     </div>
-    
-  )
+  );
 }
 
-export default Slidebarright
+export default Slidebarright;
